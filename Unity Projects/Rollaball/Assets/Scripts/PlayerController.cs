ec189bf00bb5102a32d8824ff10991476674c4ff
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-03-13 17:38:04 -0600

    More rollaball tutorial

--- Unity Projects/Rollaball/Assets/Scripts/PlayerController.cs
+++ Unity Projects/Rollaball/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    [SerializeField] public float speed;
+    public TextMeshProUGUI countText;
     
     private Rigidbody _rb;
+    private int _count;
 
     private float _movementX;
     private float _movementY;
@@ -13,7 +17,10 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _count = 0;
         _rb = GetComponent<Rigidbody>();
+        
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -22,11 +29,27 @@ public class PlayerController : MonoBehaviour
         _rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            SetCountText();
+            other.gameObject.SetActive(false);
+            _count = _count + 1;
+        }
+        
+    }
+
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         _movementX = movementVector.x;
         _movementY = movementVector.y;
     }
- 
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + _count.ToString();
+    }
+    
 }
--- Unity Projects/Rollaball/Assets/Scripts/Rotator.cs
+++ Unity Projects/Rollaball/Assets/Scripts/Rotator.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate the object on x, y , and z axies by specified amounts, adjusted for framerate
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
