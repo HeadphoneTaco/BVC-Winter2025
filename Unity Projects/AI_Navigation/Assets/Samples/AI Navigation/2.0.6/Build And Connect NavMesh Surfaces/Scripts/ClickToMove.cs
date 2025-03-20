fbc092be8e60c017b1241d550ebc19206165b2f3
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-03-20 14:48:41 -0600

    Updated ClickToMove.cs

--- Unity Projects/AI_Navigation/Assets/Samples/AI Navigation/2.0.6/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
+++ Unity Projects/AI_Navigation/Assets/Samples/AI Navigation/2.0.6/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,12 +10,14 @@ namespace Unity.AI.Navigation.Samples
     [RequireComponent(typeof(NavMeshAgent))]
     public class ClickToMove : MonoBehaviour
     {
-        NavMeshAgent m_Agent;
-        RaycastHit m_HitInfo = new RaycastHit();
-    
+        private NavMeshAgent m_Agent;
+        private RaycastHit m_HitInfo;
+        private Animator m_Animator;
+        
         void Start()
         {
             m_Agent = GetComponent<NavMeshAgent>();
+            m_Animator =GetComponent<Animator>();
         }
     
         void Update()
@@ -24,6 +27,23 @@ namespace Unity.AI.Navigation.Samples
                 var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
                     m_Agent.destination = m_HitInfo.point;
+            }
+
+            if (m_Agent.velocity.magnitude != 0f)
+            {
+                m_Animator.SetBool("Running", true);
+            }
+            else
+            {
+                m_Animator.SetBool("Running", false);
+            }
+        }
+
+        private void OnAnimatorMove()
+        {
+            if (m_Animator.GetBool("Running"))
+            {
+                m_Agent.speed = (m_Animator.deltaPosition / Time.deltaTime).magnitude;
             }
         }
     }
